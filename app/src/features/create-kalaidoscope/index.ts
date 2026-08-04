c6647ca8684277e24a36c96c3b12458a76b8f5a5
louiseightsix <59860473+louiseightsix@users.noreply.github.com>
2026-08-04 12:24:47 -0400

    remove template selection from scope creation (#7)

--- app/src/features/create-kalaidoscope/index.ts
+++ app/src/features/create-kalaidoscope/index.ts
@@ -1,4 +1,3 @@
-export { default as SelectTemplate } from "./pages/SelectTemplate";
 export { default as KalaidoscopeSetup } from "./pages/KalaidoscopeSetup";
 export { IconPicker } from "./components/icon-picker";
 export { NavKalaidoscopeSwitcher } from "./components/nav-kalaidoscope-switcher";
