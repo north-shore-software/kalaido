38d8cab528f49ee45d84fd353505073d4013111a
louiseightsix <59860473+louiseightsix@users.noreply.github.com>
2026-09-02 08:35:19 -0700

    map 6

--- kalaidoscope/internal/mapping/aggregate.go
+++ kalaidoscope/internal/mapping/aggregate.go
@@ -3,26 +3,47 @@ package mapping
 import (
 	"log"
 	"sync"
+	"time"
 
 	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase/core"
 )
 
-var aggregateSignal = make(chan struct{}, 1)
+const (
+	consolidatePendingFloor = 50
+	consolidateStaleAge     = time.Minute
+	aggregateTick           = 10 * time.Second
+)
 
 var aggregateMu sync.Mutex
 
-func signalAggregate() {
-	select {
-	case aggregateSignal <- struct{}{}:
-	default:
+func aggregateLoop() {
+	for range time.Tick(aggregateTick) {
+		due, err := consolidateDue(workerApp, time.Now())
+		if err != nil {
+			log.Printf("mapping: consolidate check: %v", err)
+			continue
+		}
+		if due {
+			cycle(workerApp)
+			continue
+		}
+		if err := refreshCounters(workerApp); err != nil {
+			log.Printf("mapping: counters: %v", err)
+		}
 	}
 }
 
-func aggregateLoop() {
-	for range aggregateSignal {
-		cycle(workerApp)
+func consolidateDue(app core.App, now time.Time) (bool, error) {
+	rows, err := app.FindRecordsByFilter("fragment_annotation", "folded = false", "-created", 0, 0, nil)
+	if err != nil || len(rows) == 0 {
+		return false, err
 	}
+	if len(rows) > consolidatePendingFloor {
+		return true, nil
+	}
+	newest := rows[0].GetDateTime("created").Time()
+	return now.Sub(newest) > consolidateStaleAge, nil
 }
 
 func settle(app core.App) {
@@ -52,6 +73,9 @@ func refreshCounters(app core.App) error {
 	d, err := loadDocument(app)
 	if err != nil {
 		return err
+	}
+	if d.rec.GetInt("fragments") == int(fragments) && d.rec.GetInt("annotated") == int(annotated) {
+		return nil
 	}
 	d.rec.Set("fragments", fragments)
 	d.rec.Set("annotated", annotated)
